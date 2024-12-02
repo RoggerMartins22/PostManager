2dd77ca82441e955346cb38efe712d4b7df0e6d6
rogger <rogger@gmail.com>
2024-12-01 23:59:32 -0300

    Implementando serviço de cache redis

--- database.py
+++ database.py
@@ -2,7 +2,6 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from models.usuario import Base
 DATABASE_URL = "postgresql://root:22082003@db:5432/postagens"
-#db_redis = redis.Redis(host='localhost', port=6379, db=0, decode_responses=True)
 
 engine = create_engine(DATABASE_URL)
 
