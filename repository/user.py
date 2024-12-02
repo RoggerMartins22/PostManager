b49bb36dd87c3cf43ae8f52b81f5fd44e8e9e131
rogger <rogger@gmail.com>
2024-12-02 08:42:59 -0300

    Adicionando mensagem de retorno ao tentar criar usuario com email ja existente

--- repository/user.py
+++ repository/user.py
@@ -1,4 +1,5 @@
 from sqlalchemy.orm import Session
+from sqlalchemy.exc import IntegrityError
 from passlib.context import CryptContext
 from models.user import Users
 from schemas.user import UserCreate
@@ -10,11 +11,14 @@ class UserRepository:
     def create_user_repository(db: Session, user: UserCreate):
         db_user = Users(email=user.email, nome=user.nome, senha_hashed=user.senha)
         
-        db.add(db_user)
-        db.commit()
-        db.refresh(db_user)
-        
-        return db_user
+        try:
+            db.add(db_user)
+            db.commit()
+            db.refresh(db_user)
+            return db_user
+        except IntegrityError as e:
+            db.rollback()
+            raise e
 
     @staticmethod
     def get_user_by_email(db: Session, email: str):
--- services/user.py
+++ services/user.py
@@ -1,4 +1,6 @@
 from sqlalchemy.orm import Session
+from fastapi import HTTPException, status
+from sqlalchemy.exc import IntegrityError
 from repository.user import UserRepository
 from schemas.user import UserCreate
 from passlib.context import CryptContext
@@ -9,4 +11,11 @@ class UserService:
     def create_user_service(db: Session, user: UserCreate):
         hashed_password = pwd_context.hash(user.senha)
         user.senha = hashed_password
-        return UserRepository.create_user_repository(db=db, user=user)
+        
+        try:
+            return UserRepository.create_user_repository(db=db, user=user)
+        except IntegrityError:
+            raise HTTPException(
+                status_code=status.HTTP_400_BAD_REQUEST,
+                detail="Já existe um usuário com este e-mail cadastrado."
+            )
